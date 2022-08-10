76c9ad09a2e7e8d46f53a0875ea0a81d966c304f
TojeMneCiceron <skorobogatova.mm5910@gmail.com>
2022-08-10 22:09:49 +0500

    final

---
+++ binary_heap/Heap.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace binary_heap
+{
+    class HeapNode<T>
+    {
+        int key;
+        T value;
+
+        public HeapNode(int key, T value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+
+        public int Key { get => key; set => key = value; }
+        public T Value { get => value; set => this.value = value; }
+
+        public override string ToString()
+        {
+            return $"{key}({value})";
+        }
+    }
+    class Heap<T>
+    {
+        int size, n;
+        HeapNode<T>[] nodes;
+
+        public Heap(int size)
+        {
+            this.size = size;
+            n = 0;
+            nodes = new HeapNode<T>[size + 1];
+        }
+        public static void Swap(ref HeapNode<T> a, ref HeapNode<T> b)
+        {
+            HeapNode<T> temp = a;
+            a = b;
+            b = temp;
+        }
+        public HeapNode<T> Min()
+        {
+            return nodes[1];
+        }
+        public bool Insert(int new_key, T new_value)
+        {
+            if (n >= size)
+                return false;
+
+            n++;
+            nodes[n] = new HeapNode<T>(new_key, new_value);
+
+            for (int i = n; i > 1 && nodes[i].Key > nodes[i / 2].Key; i /= 2)
+                Swap(ref nodes[i], ref nodes[i / 2]);
+
+            return true;
+        }
+        public HeapNode<T> DeleteMin()
+        {
+            if (n == 0)
+                return null;
+
+            HeapNode<T> min = nodes[1];
+
+            nodes[1] = nodes[n];
+            n--;
+
+            Heapify(1);
+
+            return min;
+        }
+        void Heapify(int i)
+        {
+            for (; ; )
+            {
+                int left = 2 * i;
+                int right = 2 * i + 1;
+
+                int min = i;
+
+                if (left <= n && nodes[left].Key < nodes[right].Key && nodes[left].Key < nodes[i].Key)
+                { 
+                    min = left; 
+                }
+
+                if (right <= n && nodes[right].Key < nodes[left].Key && nodes[left].Key < nodes[i].Key)
+                { 
+                    min = right;
+                }
+
+                if (min == i)
+                    break;
+
+                Swap(ref nodes[i], ref nodes[min]);
+                i = min;
+            }
+        }
+        public override string ToString()
+        {
+            if (n == 0)
+                return "";
+
+            string res = $"{nodes[1].Key}({nodes[1].Value})\n";
+
+            int a = 4;
+
+            for (int i = 2; i <= n; i++)
+            {
+                res += $"{nodes[i].Key}({nodes[i].Value}) ";
+
+                if (i + 1 == a)
+                {
+                    res += "\n";
+                    a *= 2;
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ binary_heap/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.IO;
+
+namespace binary_heap
+{
+    class Program
+    {
+        public static void GenerateHeap(int size, int n, int max)
+        {
+            StreamWriter writer = new StreamWriter(@"C:\Users\Пользователь\source\repos\binary_heap\binary_heap\output.txt");
+
+            Heap<char> heap = new Heap<char>(size);
+
+            Random rand = new Random();
+
+            char c = 'a';
+
+            writer.WriteLine("\n---------------------Creating heap---------------------\n");
+
+            for (int i = 0; i < n; i++)
+            {
+                if (!heap.Insert(rand.Next(1, max), c))
+                {
+                    writer.WriteLine("\nКуча переполнена\n");
+                    break;
+                }
+
+                writer.WriteLine($"\n{heap}\n");
+
+                c++;
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                writer.WriteLine("\n---------------------Finding min---------------------\n");
+
+                HeapNode<char> heapNode = heap.Min();
+
+                if (heapNode is null)
+                {
+                    writer.WriteLine("The heap is empty");
+                    break;
+                }
+                writer.WriteLine($"\nMin: {heapNode}\n");
+
+                writer.WriteLine("\n---------------------Deleting min---------------------\n");
+
+                heapNode = heap.DeleteMin();
+
+                writer.WriteLine($"\nDeleted node: {heapNode}\n");
+
+                writer.WriteLine($"\n{heap}\n");
+            }
+
+            writer.Close();
+        }
+
+        public static void Insert(ref Heap<string> heap)
+        {
+            Console.WriteLine("\n---------------------Inserting node---------------------\n");
+
+            Console.WriteLine("Enter key");
+            int key = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value");
+            string value = Console.ReadLine();
+
+            if (!heap.Insert(key, value))
+            {
+                Console.WriteLine("\nКуча переполнена\n");
+                return;
+            }
+
+            Console.WriteLine($"\n{heap}\n");
+        }
+
+        public static void FindMin(Heap<string> heap)
+        {
+            Console.WriteLine("\n---------------------Finding min---------------------\n");
+
+            HeapNode<string> heapNode = heap.Min();
+
+            if (heapNode is null)
+            {
+                Console.WriteLine("The heap is empty");
+                return;
+            }
+
+            Console.WriteLine($"\nMin: {heapNode}\n");
+        }
+
+        public static void DeleteMin(ref Heap<string> heap)
+        {
+            Console.WriteLine("\n---------------------Deleting min---------------------\n");
+
+            HeapNode<string> heapNode = heap.DeleteMin();
+
+            if (heapNode is null)
+            {
+                Console.WriteLine("The heap is empty");
+                return;
+            }
+
+            Console.WriteLine($"\nDeleted node: {heapNode}\n");
+
+            Console.WriteLine($"\n{heap}\n");
+        }
+
+        public static void CreateHeap(int size)
+        {
+            Heap<string> heap = new Heap<string>(size);
+
+            while (true)
+            {
+                string ans = "";
+                Console.WriteLine("\nChoose operation:\n1 - Insert\n2 - Find min\n3 - Delete min\n");
+                ans = Console.ReadLine();
+
+                if (ans == "1")
+                {
+                    Insert(ref heap);
+                }
+                else
+                if (ans == "2")
+                {
+                    FindMin(heap);
+                }
+                else
+                if (ans == "3")
+                {
+                    DeleteMin(ref heap);
+                }
+                else
+                {
+                    continue;
+                }
+
+                Console.WriteLine("\nContinue? y/n");
+                ans = Console.ReadLine();
+                if (ans == "n")
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string ans = "";
+                Console.WriteLine("Choose way of creation:\n1 - Console\n2 - Generate\n");
+                ans = Console.ReadLine();
+
+                if (ans == "1")
+                {
+                    Console.WriteLine("Enter heap size");
+                    int size = Convert.ToInt32(Console.ReadLine());
+
+                    CreateHeap(size);
+                    break;
+                }
+                else
+                if (ans == "2")
+                {
+                    Console.WriteLine("Enter heap size");
+                    int size = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Enter nodes count");
+                    int n = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Enter max key value");
+                    int max = Convert.ToInt32(Console.ReadLine());
+
+                    GenerateHeap(size, n, max);
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
